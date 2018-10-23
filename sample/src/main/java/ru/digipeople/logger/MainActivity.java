8524dfa62cb4e4c169525978108e5f3456d39bf1
Aleksandr Brazhkin <brajkin@digipeople.ru>
2018-10-23 18:43:54 +0300

    Initial commit

---
+++ sample/src/main/java/ru/digipeople/logger/MainActivity.java
@@ -0,0 +1,9 @@
+package ru.digipeople.logger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author Aleksandr Brazhkin
+ */
+public class MainActivity extends AppCompatActivity {
+}
